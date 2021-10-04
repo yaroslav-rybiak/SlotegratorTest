117617002c8fefaa715449f67a3d3a96067e90bd
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2021-10-05 00:29:49 +0300

    adding AUTHORIZATION constant

---
+++ src/main/java/com/slotegrator/params/Constants.java
@@ -0,0 +1,20 @@
+package com.slotegrator.params;
+
+public class Constants {
+
+    public final static String BASE_URI = "http://test-app.d6.dev.devcaz.com/";
+    public final static String API_URL = "http://test-api.d6.dev.devcaz.com";
+    public final static String LOGIN_PATH = "admin/login";
+    public final static String LOGIN_URL = BASE_URI + LOGIN_PATH;
+    public final static String DASHBOARD_PATH = "configurator/dashboard/index";
+    public final static String DASHBOARD_URL = BASE_URI + DASHBOARD_PATH;
+    public final static String PLAYERS_PATH = "user/player/admin";
+    public final static String PLAYERS_URL = BASE_URI + PLAYERS_PATH;
+    public final static String ADMIN_LOGIN = "admin1";
+    public final static String ADMIN_PASSWORD = "[9k<k8^z!+$$GkuP";
+    public static final String CLIENT_CREDENTIALS_GRANT = "/v2/oauth2/token";
+    public static final String AUTHENTICATION_USERNAME = "front_2d6b0a8391742f5d789d7d915755e09e:";
+    public static final String REGISTRATION = "/v2/players";
+    public static final String GET_PROFILE = "/v2/players/";
+    public static final String AUTHORIZATION = "/v2/oauth2/token";
+}
